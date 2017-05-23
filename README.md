0a510d624eea671974ac332355d6e352306865e5
Dom Wakeling <domwakeling@gmail.com>
2017-05-23 13:14:29 +0000

    Continue setting up base FCC Clementine.js
    Still requires Passport integration client-side.
    README updated.

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # Clementine.js
 
-This is a starter template, equivalent to the FreeCodeCamp version of the GitHub repository.
+This is a starter template, equivalent to the FreeCodeCamp version of the GitHub repository,
+built from scratch following these tutorials:
+
+* [Clementine.js 'beginner' tutorial](http://www.clementinejs.com/tutorials/tutorial-beginner.html)
+* [Clementine.js 'integrate Passport' tutorial](http://www.clementinejs.com/tutorials/tutorial-passport.html) 
+
+A current version is also available directly at https://github.com/johnstonbl01/clementinejs-fcc
--- server.js
+++ server.js
@@ -2,18 +2,30 @@
 
 var express = require('express'),
     routes = require('./app/routes/index.js'),
-    mongoose = require('mongoose');
+    mongoose = require('mongoose'),
+    passport = require('passport'),
+    session = require('express-session');
 
 var app = express();
 
-require('dotenv').config();
+require('dotenv').load(); // was config
+require('./app/config/passport')(passport);
 
 mongoose.connect(process.env.MONGO_URI);
 
 app.use('/public', express.static(process.cwd() + '/public'));
 app.use('/controllers', express.static(process.cwd() + '/app/controllers'));
 
-routes(app);
+app.use(session({
+    secret: 'secretClementine',
+    resave: false,
+    saveUninitialized: true
+}))
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+routes(app, passport);
 
 app.listen(process.env.PORT, function () {
     console.log('Listening on port ' + process.env.PORT + ' ...');
